d9766bd9c17854242fa28776ccd33bba22617674
Musha02 <89347202+Musha02@users.noreply.github.com>
2024-04-29 23:46:53 +0530

    updated

--- src/app/typescript/introduction/page.tsx
+++ src/app/typescript/introduction/page.tsx
@@ -20,30 +20,14 @@ export default function Introduction() {
             TypeScript stands in an unusual relationship to JavaScript. TypeScript offers all of JavaScript’s features, and an additional layer on top of these: TypeScript’s type system.
             </p>
           </Balancer>
-
-          {/* Image grid */}
-        <div className="grid grid-cols-3 gap-6 mt-8">
-          {/* Image 1 */}
-          <img src="/image1.jpg" alt="Image 1" className="w-full h-auto rounded-lg" />
-          {/* Image 2 */}
-          <img src="/image2.jpg" alt="Image 2" className="w-full h-auto rounded-lg" />
-          {/* Image 3 */}
-          <img src="/image3.jpg" alt="Image 3" className="w-full h-auto rounded-lg" />
-          {/* Image 4 */}
-          <img src="/image4.jpg" alt="Image 4" className="w-full h-auto rounded-lg" />
-          {/* Image 5 */}
-          <img src="/image5.jpg" alt="Image 5" className="w-full h-auto rounded-lg" />
-          {/* Image 6 */}
-          <img src="/image6.jpg" alt="Image 6" className="w-full h-auto rounded-lg" />
-        </div>
           
           <div className="flex justify-center mt-4">
-            <button className="border-black w-fit rounded-xl border-2 bg-black px-4 py-2 text-white transition-all hover:border-black hover:bg-black hover:bg-transparent hover:text-black/90 mr-4">
-              <a href="/">Home</a>
-            </button>
-            <button className="border-black w-fit rounded-xl border-2 bg-black px-4 py-2 text-white transition-all hover:border-black hover:bg-black hover:bg-transparent hover:text-black/90">
-              <a href="/nextjs/project-structure">Next Page</a>
-            </button>
+          < Link href= "/"  className="border-black w-fit rounded-xl border-2 bg-black px-4 py-2 text-white transition-all hover:border-black hover:bg-black hover:bg-transparent hover:text-black/90 mr-4">
+             Home
+            </Link>
+            <Link href="/typescript" className="border-black w-fit rounded-xl border-2 bg-black px-4 py-2 text-white transition-all hover:border-black hover:bg-black hover:bg-transparent hover:text-black/90">
+              Next Page
+            </Link>
           </div>
           
           </div>
