e03e4a4d0652847c385634e7a44a06f76c24bb51
Musha02 <89347202+Musha02@users.noreply.github.com>
2024-04-29 23:47:14 +0530

    updated

--- src/app/typescript/page.tsx
+++ src/app/typescript/page.tsx
@@ -20,13 +20,12 @@ export default function NextJs() {
             <h2 className=" text-neutral-400 md:max-w-[700px] md:text-2xl hover:text-black/90">3. <Link href="/typescript/introduction">Rewriting javascript code to typescript</Link></h2>
             <h2 className=" text-neutral-400 md:max-w-[700px] md:text-2xl hover:text-black/90">4. <Link href="/typescript/introduction">React with Typescript</Link></h2>
             </Balancer>
-          <button className="border-balck  w-fit rounded-xl border-2 bg-black px-4 py-2  text-white transition-all hover:border-black hover:bg-black hover:bg-transparent  hover:text-black/90">
-            <a href="/">
+            <Link href="/" className="border-balck  w-fit rounded-xl border-2 bg-black px-4 py-2  text-white transition-all hover:border-black hover:bg-black hover:bg-transparent  hover:text-black/90">
               Home
-            </a>
-          </button>
+            </Link>
           
           </div>
+
         </section>
       </div>
     
