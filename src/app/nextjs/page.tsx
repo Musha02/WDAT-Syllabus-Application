445a8274f053c94ea27bb6b5708f7029788b93d8
Musha02 <89347202+Musha02@users.noreply.github.com>
2024-04-29 23:46:01 +0530

    Changed Button as Link

--- src/app/nextjs/page.tsx
+++ src/app/nextjs/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Navbar from "@/components/navbar";
 import Balancer from "react-wrap-balancer";
+import Image from "next/image";
+import Next from "@/assets/images/Next.jpeg"
+
 
 export default function NextJs() {
   return (
@@ -25,13 +28,23 @@ export default function NextJs() {
             <h2 className=" text-neutral-400 md:max-w-[700px] md:text-2xl hover:text-black/90">8. <Link href="/nextjs/optimizing">Optimizing</Link></h2>
             <h2 className=" text-neutral-400 md:max-w-[700px] md:text-2xl hover:text-black/90">9. <Link href="/nextjs/testing">Testing</Link></h2>
           </Balancer>
-          <button className="border-balck  w-fit rounded-xl border-2 bg-black px-4 py-2  text-white transition-all hover:border-black hover:bg-black hover:bg-transparent  hover:text-black/90">
-            <a href="/">
+          <Link href="/" className="border-balck  w-fit rounded-xl border-2 bg-black px-4 py-2  text-white transition-all hover:border-black hover:bg-black hover:bg-transparent  hover:text-black/90">
               Home
-            </a>
-          </button>
+          </Link>
           
           </div>
+          <section className="md:w-1/2 mt-20 ">
+          <Image
+            className="hidden h-auto max-w-[600px] mt-20 md:block rounded-3xl"
+            src={Next}
+            alt="next-image"
+          />
+          <Image
+            className="h-auto w-full  md:hidden"
+            src={Next}
+            alt="next-image"
+          />
+         </section>
         </section>
       </div>
     
