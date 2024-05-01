573f80d1817c9efdbdce52dd12aaceba273efbb7
Musha02 <89347202+Musha02@users.noreply.github.com>
2024-05-01 23:47:45 +0530

    Update Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,13 +2,13 @@
 
 import Image from "next/image";
 import { useState } from "react";
-
 import Mylogo from "@/assets/images/Mylogo.png";
 import Link from "next/link";
 import { IoIosArrowDown } from "react-icons/io";
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineClose } from "react-icons/ai";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 type NavItem = {
   label: string;
@@ -87,6 +87,8 @@ const navItems: NavItem[] = [
 export default function Navbar() {
   const [animationParent] = useAutoAnimate();
   const [isSideMenuOpen, setSideMenue] = useState(false);
+  const { user } = useUser();
+
   function openSideMenu() {
     setSideMenue(true);
   }
@@ -96,82 +98,103 @@ export default function Navbar() {
 
   return (
     <div className="mx-auto flex  w-full max-w-7xl text-xl justify-between px-4 py-5 text-sm">
-      {/* left side  */}
-      <section ref={animationParent} className="flex items-center gap-10">
-        {/* logo */}
-        <Image src={Mylogo} alt=" logo" className="w-28 h-14" />
-        {isSideMenuOpen && <MobileNav closeSideMenu={closeSideMenu} />}
-        <div className="hidden md:flex items-center gap-4 transition-all">
-          {navItems.map((d, i) => (
-            <Link
-              key={i}
-              href={d.link ?? "#"}
-              className="relative group  px-2 py-3 transition-all "
-            >
-              <p className="flex cursor-pointer items-center gap-2 text-neutral-400 group-hover:text-black ">
-                <span>{d.label}</span>
-                {d.children && (
-                  <IoIosArrowDown className=" rotate-180  transition-all group-hover:rotate-0" />
-                )}
-              </p>
-
-              {/* dropdown */}
+    {/* left side  */}
+    <section ref={animationParent} className="flex items-center gap-10">
+      {/* logo */}
+      <Image src={Mylogo} alt=" logo" className="w-28 h-14" />
+      {isSideMenuOpen && <MobileNav closeSideMenu={closeSideMenu} />}
+      <div className="hidden md:flex items-center gap-4 transition-all">
+        {navItems.map((d, i) => (
+          <Link
+            key={i}
+            href={d.link ?? "#"}
+            className="relative group  px-2 py-3 transition-all "
+          >
+            <p className="flex cursor-pointer items-center gap-2 text-neutral-400 group-hover:text-black ">
+              <span>{d.label}</span>
               {d.children && (
-                <div className="absolute   right-0   top-10 hidden w-auto  flex-col gap-1   rounded-lg bg-white py-3 shadow-md  transition-all group-hover:flex ">
-                  {d.children.map((ch, i) => (
-                    <Link
-                      key={i}
-                      href={ch.link ?? "#"}
-                      className=" flex cursor-pointer items-center  py-1 pl-6 pr-8  text-neutral-400 hover:text-black  "
-                    >
-                      {/* image */}
-                      {ch.iconImage && (
-                        <Image src={ch.iconImage} alt="item-icon" />
-                      )}
-                      {/* item */}
-                      <span className="whitespace-nowrap   pl-3 ">
-                        {ch.label}
-                      </span>
-                    </Link>
-                  ))}
-                </div>
+                <IoIosArrowDown className=" rotate-180  transition-all group-hover:rotate-0" />
               )}
+            </p>
+
+            {/* dropdown */}
+            {d.children && (
+              <div className="absolute   right-0   top-10 hidden w-auto  flex-col gap-1   rounded-lg bg-white py-3 shadow-md  transition-all group-hover:flex ">
+                {d.children.map((ch, i) => (
+                  <Link
+                    key={i}
+                    href={ch.link ?? "#"}
+                    className=" flex cursor-pointer items-center  py-1 pl-6 pr-8  text-neutral-400 hover:text-black  "
+                  >
+                    {/* image */}
+                    {ch.iconImage && (
+                      <Image src={ch.iconImage} alt="item-icon" />
+                    )}
+                    {/* item */}
+                    <span className="whitespace-nowrap   pl-3 ">
+                      {ch.label}
+                    </span>
+                  </Link>
+                ))}
+              </div>
+            )}
+          </Link>
+        ))}
+      </div>
+      {/* navitems */}
+    </section>
+
+    {/* right side data */}
+    <section className="hidden md:flex items-center gap-8">
+        {user ? (
+          <>
+            <span>{`Hi ${user.nickname}!`}</span>
+            <Link href="/api/auth/logout" className="h-fit rounded-xl text-xl border-2 border-neutral-400 px-4 py-2 text-neutral-400 transition-all hover:border-black hover:text-black/90">
+              Logout
             </Link>
-          ))}
-        </div>
-        {/* navitems */}
+          </>
+        ) : (
+          <Link href="/api/auth/login" className="h-fit rounded-xl text-xl border-2 border-neutral-400 px-4 py-2 text-neutral-400 transition-all hover:border-black hover:text-black/90">
+            Login
+          </Link>
+        )}
       </section>
 
-      {/* right side data */}
-      <section className=" hidden md:flex   items-center gap-8 ">
-        <button className="h-fit text-neutral-400 text-xl transition-all hover:text-black/90">
-          Login
-        </button>
-
-        <button className="h-fit rounded-xl text-xl border-2 border-neutral-400 px-4 py-2 text-neutral-400 transition-all hover:border-black hover:text-black/90">
-          Register
-        </button>
-      </section>
-
-      <FiMenu
-        onClick={openSideMenu}
-        className="cursor-pointer text-4xl md:hidden"
-      />
-    </div>
-  );
+    <FiMenu
+      onClick={openSideMenu}
+      className="cursor-pointer text-4xl md:hidden"
+    />
+  </div>
+)
 }
 
 function MobileNav({ closeSideMenu }: { closeSideMenu: () => void }) {
+  const { user } = useUser();
   return (
-    <div className="fixed left-0 top-0 flex h-full min-h-screen w-full justify-end bg-black/60 md:hidden">
-      <div className=" h-full w-[65%] bg-white px-4 py-4">
-        <section className="flex justify-end">
+    <div className="fixed left-0 top-0 flex h-full min-h-screen w-full justify-end bg-black/60 md:hidden ">
+      <div className=" h-full w-[65%] bg-white px-4 py-4 ">
+      <section className="flex justify-end ">
           <AiOutlineClose
             onClick={closeSideMenu}
-            className="cursor-pointer text-4xl "
+            className="cursor-pointer text-3xl "
           />
+      </section>
+      <section className="flex flex-col gap-8  mt-4 items-center">
+        {user ? (
+          <>
+            <span>{`Hi ${user.nickname}!`}</span>
+            <Link href="/api/auth/logout" className="h-fit rounded-xl text-xl border-2 border-neutral-400 px-4 py-1 text-neutral-400 transition-all hover:border-black hover:text-black/90">
+              Logout
+            </Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login" className="h-fit rounded-xl text-xl border-2 border-neutral-400 px-4 py-1 text-neutral-400 transition-all hover:border-black hover:text-black/90">
+            Login
+          </Link>
+        )}
         </section>
-        <div className=" flex flex-col text-base  gap-2 transition-all">
+        
+        <div className=" flex flex-col text-base gap-2 text-neutral-400 transition-all mt-6 hover:border-black hover:text-black/90">
           {navItems.map((d, i) => (
             <SingleNavItem
               key={i}
@@ -184,15 +207,7 @@ function MobileNav({ closeSideMenu }: { closeSideMenu: () => void }) {
           ))}
         </div>
 
-        <section className="  flex  flex-col   gap-8  mt-4 items-center">
-          <button className="h-fit text-neutral-400 transition-all hover:text-black/90">
-            Login
-          </button>
-
-          <button className="w-full  max-w-[200px]  rounded-xl border-2 border-neutral-400 px-4 py-2 text-neutral-400 transition-all hover:border-black hover:text-black/90">
-            Register
-          </button>
-        </section>
+        
       </div>
     </div>
   );
